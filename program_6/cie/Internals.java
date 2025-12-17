6b90c133d4493cbe5f9e89769ea1f37c294bbe8b
priyankarajput-1BF24CS234 <priyankarajput.cs24@bmsce.ac.in>
2025-12-17 19:46:46 +0530

    Update Internals.java

--- program_6/cie/Internals.java
+++ program_6/cie/Internals.java
@@ -1,1 +1,12 @@
+package cie;
+
+public class Internals {
+    protected int internalMarks[] = new int[5];
+
+    public Internals(int marks[]) {
+        for (int i = 0; i < 5; i++) {
+            internalMarks[i] = marks[i];
+        }
+    }
+}
 
