9e124d2c2c041eba3f16bb1912469dfafb79b9af
priyankarajput-1BF24CS234 <priyankarajput.cs24@bmsce.ac.in>
2025-12-17 19:47:04 +0530

    Update Personal.java

--- program_6/cie/Personal.java
+++ program_6/cie/Personal.java
@@ -1,1 +1,14 @@
+package cie;
+
+public class Personal {
+    protected String usn;
+    protected String name;
+    protected int sem;
+
+    public Personal(String usn, String name, int sem) {
+        this.usn = usn;
+        this.name = name;
+        this.sem = sem;
+    }
+}
 
